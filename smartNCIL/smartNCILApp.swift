11ff1c72cd671977ba0f44598b2d9314f12178f2
Rahul-Routh <its.rahulrouth@gmail.com>
2025-08-27 08:53:29 +0530

    Initial Commit

---
+++ smartNCIL/smartNCILApp.swift
@@ -0,0 +1,17 @@
+//
+//  smartNCILApp.swift
+//  smartNCIL
+//
+//  Created by Rahul Routh on 27/08/25.
+//
+
+import SwiftUI
+
+@main
+struct smartNCILApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ smartNCILTests/smartNCILTests.swift
@@ -0,0 +1,17 @@
+//
+//  smartNCILTests.swift
+//  smartNCILTests
+//
+//  Created by Rahul Routh on 27/08/25.
+//
+
+import Testing
+@testable import smartNCIL
+
+struct smartNCILTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
